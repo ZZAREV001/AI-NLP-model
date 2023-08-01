b78c59b8aad74b9d423ee3a3feca3ba274aaaa5f
ZZAREV001 <zarev.zahari@gmail.com>
2023-08-01 22:16:31 +0200

    correction of the reinforce_sample method and the forward() method in the transformer.jl

--- models/rl_agent.jl
+++ models/rl_agent.jl
@@ -24,14 +24,23 @@ struct ReinforceRLAgent <: RLAgent
     policy_net::Net  # Neural network 
     value_net::Net   # Neural network
     optimizer::Optimizer
-    ...
+    encoder::TransformerEncoder
+    decoder::TransformerDecoder
 end
 
-function reinforce_sample(agent::ReinforceRLAgent, state)
-    probs = agent.policy_net(state) 
-    sample = sample(1:size(probs,2), Weights(probs))
-    return sample 
+function reinforce_sample(agent::ReinforceRLAgent, encoder_output, decoder_output)
+    # Concatenate encoder and decoder outputs
+    state = cat(encoder_output, decoder_output, dims=2)
+
+    # Pass state through policy network to get action probabilities
+    probs = softmax(agent.policy_net(state))
+
+    # Sample an action from the action probabilities
+    action = sample(probs)
+
+    return action
 end
+
 
 function update_reinforce!(agent::ReinforceRLAgent, rewards) 
     ...
--- models/transformer.jl
+++ models/transformer.jl
@@ -39,12 +39,16 @@ function forward(model::Transformer, src, trg, mode)
     # Initialize an empty tensor to hold the decoder outputs
     decoder_outputs = []
 
+    # Initialize an empty tensor to hold the attention weights
+    attention_weights = []
+
     # Depending on the mode, we may use teacher forcing or not
     if mode == "teacher_forcing"
         for t in 2:size(trg, 2)
             # Pass the encoder output and the decoder input through the decoder
-            decoder_output, decoder_state = forward(model.decoder, decoder_input, encoder_output)
+            decoder_output, decoder_state, attn_weights = forward(model.decoder, decoder_input, encoder_output)
             push!(decoder_outputs, decoder_output)
+            push!(attention_weights, attn_weights)
 
             # Use the actual next token as the next input to the decoder
             decoder_input = trg[:, t]
@@ -52,8 +56,9 @@ function forward(model::Transformer, src, trg, mode)
     elseif mode == "feedback"
         for t in 2:size(trg, 2)
             # Pass the encoder output and the decoder input through the decoder
-            decoder_output, decoder_state = forward(model.decoder, decoder_input, encoder_output)
+            decoder_output, decoder_state, attn_weights = forward(model.decoder, decoder_input, encoder_output)
             push!(decoder_outputs, decoder_output)
+            push!(attention_weights, attn_weights)
 
             # Use the decoder output as the next input to the decoder
             decoder_input = argmax(decoder_output, dims=2)
@@ -62,11 +67,11 @@ function forward(model::Transformer, src, trg, mode)
         error("Invalid mode: $mode")
     end
 
-    # Pass the decoder outputs through the RL agent
+    # Pass the decoder outputs and attention weights through the RL agent
     if model.rl_agent isa UniformRLAgent
         rl_output = uniform_sample(model.rl_agent)
     elseif model.rl_agent isa ReinforceRLAgent
-        rl_output = reinforce_sample(model.rl_agent, decoder_outputs)
+        rl_output = reinforce_sample(model.rl_agent, decoder_outputs, attention_weights)
     else
         error("Invalid RL agent: $(typeof(model.rl_agent))")
     end
